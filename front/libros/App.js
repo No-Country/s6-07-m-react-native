1a91fa51f4250d324937438ba940401803b8abba
Francisco Rey <reyf1998@outlook.es>
2023-02-06 19:04:08 -0300

    add eslint and prettier

--- front/libros/App.js
+++ front/libros/App.js
@@ -1,14 +1,14 @@
-import "react-native-gesture-handler";
+import 'react-native-gesture-handler'
 
-import * as React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import StackNavigator from "./src/router/StackNavigator";
-import TabNavigator from "./src/router/TabNavigator";
+import * as React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import StackNavigator from './src/router/StackNavigator'
+import TabNavigator from './src/router/TabNavigator'
 
-export default function App() {
+export default function App () {
   return (
-    <NavigationContainer>
-      <TabNavigator />
-    </NavigationContainer>
-  );
+		<NavigationContainer>
+			<TabNavigator />
+		</NavigationContainer>
+  )
 }
--- front/libros/src/screens/Home.jsx
+++ front/libros/src/screens/Home.jsx
@@ -1,12 +1,12 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React from 'react';
+import { Text, View } from 'react-native';
 
 const Home = () => {
-  return (
-    <View>
-      <Text>Test home</Text>
-    </View>
-  );
+	return (
+		<View>
+			<Text>Test home asd</Text>
+		</View>
+	);
 };
 
 export default Home;
