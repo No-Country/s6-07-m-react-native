0a804c5e41baad0e77961c639c2faeb5f2fc99e5
Mario Posada <marioposadamiranda@gmail.com>
2023-02-08 14:46:55 -0300

    Added HomeNavigator

---
+++ front/libros/src/screens/tabscreens/donation/Donation.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const Donation = () => {
+	return (
+		<View>
+			<Text>Donation navigation</Text>
+		</View>
+	);
+};
+
+export default Donation;
