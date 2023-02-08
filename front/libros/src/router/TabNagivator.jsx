e1c1168615d159a27f64e51b5df39ba762a99146
Mario Posada <marioposadamiranda@gmail.com>
2023-02-08 11:01:29 -0300

    Added options in tabBar

--- front/libros/src/router/TabNagivator.jsx
+++ front/libros/src/router/TabNagivator.jsx
@@ -4,30 +4,32 @@ import Chat from '../screens/Chat';
 import Donation from '../screens/Donation';
 import HomeBooks from '../screens/HomeBooks';
 import Profile from '../screens/Profile';
-import { Ionicons } from "@expo/vector-icons";
+import { Ionicons } from '@expo/vector-icons';
 
 const Tab = createBottomTabNavigator();
 
 const TabNagivator = () => {
 	return (
 		<Tab.Navigator
-			initialRouteName='HomeTab'
+			initialRouteName='HomeBooks'
 			screenOptions={{
 				headerShown: false,
+				tabBarActiveTintColor: '#8F77DB',
 			}}
 		>
-			<Tab.Screen 
-			options={{
-				
-				tabBarIcon: ({ focused }) => (
-				  <Ionicons
-					name={focused ? "book" : "book-outline"}
-					size={24}
-					color={focused ? "#8F77DB" : "#000"}
-				  />
-				),
-			  }}
-			name='HomeBooks' component={HomeBooks} />
+			<Tab.Screen
+				options={{
+					tabBarIcon: ({ focused }) => (
+						<Ionicons
+							name={focused ? 'book' : 'book-outline'}
+							size={24}
+							color={focused ? '#8F77DB' : '#000'}
+						/>
+					),
+				}}
+				name='HomeBooks'
+				component={HomeBooks}
+			/>
 			<Tab.Screen name='Profile' component={Profile} />
 			<Tab.Screen name='Donation' component={Donation} />
 			<Tab.Screen name='Chat' component={Chat} />
