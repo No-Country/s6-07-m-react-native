eaebfe62a3a48a9f5116dceb81972e374790e6ab
Mario Posada <marioposadamiranda@gmail.com>
2023-02-10 19:52:10 -0300

    Add styles bookdetail

--- front/libros/src/router/HomeStack.jsx
+++ front/libros/src/router/HomeStack.jsx
@@ -1,19 +1,21 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React from 'react';
 import HomeBooks from '../screens/tabscreens/home/HomeBooks';
-import HomeDetail from '../screens/tabscreens/home/HomeDetail';
+import BookDetail from '../screens/tabscreens/home/HomeDetail';
 
 const Stack = createNativeStackNavigator();
 
 const HomeNavigator = ({ navigation }) => {
 	return (
-		<Stack.Navigator>
+		<Stack.Navigator
+			initialRouteName='HomeDetail'
+		>
 			<Stack.Screen
 				name='Home'
 				options={{ headerShown: false }}
 				component={HomeBooks}
 			/>
-			<Stack.Screen name='HomeDetail' component={HomeDetail} />
+			<Stack.Screen name='HomeDetail' component={BookDetail} />
 		</Stack.Navigator>
 	);
 };
