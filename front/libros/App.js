04a57b3be9764d859f59130ad7e41ec4157939df
FedeMontenegro <fedejulianmontenegro@gmail.com>
2023-02-10 18:18:08 -0300

    Se creó la vista Login/Register.

--- front/libros/App.js
+++ front/libros/App.js
@@ -2,7 +2,8 @@ import 'react-native-gesture-handler';
 
 import * as React from 'react';
 import Router from './src/router/Router';
+import SwitchLoginRegister from './src/auth/switchLoginSignUp/components/SwitchLoginRegister';
 
 export default function App() {
-	return <Router />;
+	return <SwitchLoginRegister />;
 }
