734141b4bd2d95ac919c96942eaec21d9b3332fa
FedeMontenegro <fedejulianmontenegro@gmail.com>
2023-02-11 17:54:37 -0300

    Actualización en paleta de colores.

--- front/libros/src/utils/constants.js
+++ front/libros/src/utils/constants.js
@@ -3,6 +3,7 @@ export const colors = {
     secondary: "rgba(100, 39, 255, 0.4)",
     background: "#FFFFFF",
     text: "rgba(0, 0, 0, 0.9)",
+    textSecondary: "rgba(0, 0, 0, 0.1)",
     border: "",
     danger: "#EA1111",
     warning: "#757575"
