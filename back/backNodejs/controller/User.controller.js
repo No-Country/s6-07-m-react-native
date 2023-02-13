d0b90b139fbbc071dbe83cf1d4032fca467a0d44
fredy alberto varon guzman <fvaron@itfip.edu.co>
2023-02-13 15:45:48 -0500

    add service and format response in controller

--- back/backNodejs/controller/User.controller.js
+++ back/backNodejs/controller/User.controller.js
@@ -1,19 +1,22 @@
 const User = require("../models/User");
+const { findUser } = require("../services/User.service");
+const { NotFound, Ok, Error } = require("../util/HttpResponse");
 
 const getUser = async (req, res) => {
   try {
     const id = req.body.id;
-    const userFound = await User.findOne({ _id: id });
-    res.send({
-      done: true,
-      message: userFound,
-    });
+    const data = await findUser(id)
+    console.log(data, "data")
+    if(!data){
+      return NotFound(res, "Not Found User")
+    }
+    return Ok(res, data)
   } catch (error) {
-    console.log(error);
-    res.send({
-      done: false,
-      message: error,
-    });
+    console.log(error)
+    if(error.kind =='ObjectId'){
+      return Error(res, "Id is invalid")
+    }
+    return Error(res, error.message)
   }
 };
 
