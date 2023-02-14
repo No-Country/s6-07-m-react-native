f1686b598237d696267b433500caa8e6e4304852
fredy alberto varon guzman <fvaron@itfip.edu.co>
2023-02-14 15:39:18 -0500

    Buscador De Libros Por Titulos

--- back/backNodejs/router/book.js
+++ back/backNodejs/router/book.js
@@ -1,7 +1,8 @@
 const { Router } = require("express");
-const { donateBook } = require("../controller/Book.controller");
+const { donateBook, searchBook } = require("../controller/Book.controller");
 
 const router = Router();
 
 router.post("/donateBook", donateBook);
+router.get("/search/:title", searchBook)
 module.exports = router;
--- back/backNodejs/services/Book.service.js
+++ back/backNodejs/services/Book.service.js
@@ -1,9 +1,14 @@
+const Book = require("../models/Book");
 const User = require("../models/Book");
 
 const saveBook = async (data) => {
   return await data.save();
 };
-
+const searchBookByTitle = async (title) => {
+  const regex = new RegExp(title, 'i');
+  return await Book.find({title: regex})
+}
 module.exports = {
     saveBook,
+    searchBookByTitle
 };
