a7f02c671ba16c803bce996698723c1a2fdacad1
Federico Santoro <federicogabsantoro@gmail.com>
2023-02-14 12:42:35 -0300

    agregar service

--- back/backNodejs/controller/User.controller.js
+++ back/backNodejs/controller/User.controller.js
@@ -2,6 +2,7 @@ const User = require("../models/User");
 const { findUser } = require("../services/User.service");
 const { NotFound, Ok, Error } = require("../util/HttpResponse");
 
+// Buscar Usuario por ID
 const getUser = async (req, res) => {
   try {
     const data = await findUser(req.body.id);
@@ -19,4 +20,28 @@ const getUser = async (req, res) => {
   }
 };
 
-module.exports = { getUser };
+// Actualizar usuario
+const updateUser = async (req, res) => {
+  try {
+    const userFound = await findUser(req.body.id);
+    if (userFound) {
+      const emailRegex =
+        /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
+      if (emailRegex.test(req.body.email)) {
+        userFound.email = req.body.email;
+        userFound.profileImage = req.body.image;
+        const savedUser = await saveUser(userFound);
+        return Ok(res, savedUser);
+      } else {
+        return Error(res, "Email has wrong format");
+      }
+    } else {
+      return NotFound(res, "User not found");
+    }
+  } catch (error) {
+    console.log(error);
+    return Error(res, error.message);
+  }
+};
+
+module.exports = { getUser, updateUser };
--- back/backNodejs/router/user.js
+++ back/backNodejs/router/user.js
@@ -1,6 +1,5 @@
 const { Router } = require("express");
-const { getUser } = require("../controller/User.controller");
-const { updateUser } = require("../controller/UserUpdate.controller");
+const { getUser, updateUser } = require("../controller/User.controller");
 
 const router = Router();
 router.get("/getUser", getUser);
