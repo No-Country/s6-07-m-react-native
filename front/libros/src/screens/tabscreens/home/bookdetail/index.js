db29cc814fe80cfce9e49236db79a36ab77d75d9
Mario Posada <marioposadamiranda@gmail.com>
2023-02-15 22:59:02 -0300

    Added some featrure

--- front/libros/src/screens/tabscreens/home/bookdetail/index.js
+++ front/libros/src/screens/tabscreens/home/bookdetail/index.js
@@ -1,20 +1,34 @@
-import React from 'react';
-import { View, Text, TouchableOpacity, SafeAreaView } from 'react-native';
+import React, { useEffect } from 'react';
+import {
+	View,
+	Text,
+	TouchableOpacity,
+	SafeAreaView,
+	Platform,
+} from 'react-native';
 import { styles } from './styles';
-import { Book, Distance, Mapa } from './../../../../components/tabcomponents/home/index';
+import {
+	Book,
+	Distance,
+	Mapa,
+} from './../../../../components/tabcomponents/home/index';
 import { Reviews } from './../../../../components/tabcomponents/home/index';
-import {books} from './../../../../../mocks/bookObj.json'
+import { books } from './../../../../../mocks/bookObj.json';
+import { StatusBar } from 'react-native';
 
 const BookDetail = ({ navigation }) => {
-	console.log(books)
+	useEffect(() => {
+		Platform.OS === 'android' && StatusBar.setBackgroundColor('#ffffff');
+		
+	}, []);
 
 	return (
 		<SafeAreaView>
+			<StatusBar backgroundColor='#FF3D45' />
 			<Book />
-			<Reviews data={books}/>
+			<Reviews data={books} />
 			<Distance />
 			<Mapa />
-
 
 			<View style={styles.container}>
 				<Text style={styles.text}>Book</Text>
